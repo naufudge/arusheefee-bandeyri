fd9e0e3d377afe7a05965f6e01954c8f5df37123
Nauf <130215373+naufudge@users.noreply.github.com>
2024-12-09 12:27:03 +0500

    made some changes so that it displays decimal .00 if needed

--- lib/helpers.ts
+++ lib/helpers.ts
@@ -72,7 +72,7 @@ export function formatNumberWithCommas(num: number | string, withDecimals?: bool
             case 1:
             case 2:
             case 3:
-                return result
+                return `${result}.00`
             case 4:
                 numArray.splice(1, 0, ",")
                 return numArray.join("") + (withDecimals ? ".00": "")
