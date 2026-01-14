40fcc3029f93a2e08053cb675a9764c661a81bed
Nauf <130215373+naufudge@users.noreply.github.com>
2026-01-14 09:23:36 +0500

    fixed seeding issue

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,5 @@
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
+import { prisma } from '@/lib/prisma';
+import 'dotenv/config';
 
 const staffData = [
   { name: "Sharumeela Abdul Fatah", designation: "Accounts Officer" },
